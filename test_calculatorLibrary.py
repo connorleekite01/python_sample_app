64ffecd4132f4d60e359fe7b5a34e02d0ecd55a9
Connor Li <connorli0@outlook.com>
2023-03-07 12:00:28 -0500

    update 2

--- test_calculatorLibrary.py
+++ test_calculatorLibrary.py
@@ -6,5 +6,19 @@ class TestCalculatorLibrary(TestCase):
         # create an instance of the calculator library
         calc = CalculatorLibrary()
 
-        # test the add method
+       # test the add method with various inputs
         self.assertEqual(calc.add(2, 3), 5)
+        self.assertEqual(calc.add(10, 5), 15)
+        self.assertEqual(calc.add(2, 3), 5)
+        self.assertEqual(calc.add(10, 5), 15)
+
+    # test case for subtract method
+    def test_subtract(self):
+        # create an instance of the calculator library
+        calc = CalculatorLibrary()
+
+       # test the subtract method with various inputs
+        self.assertEqual(calc.subtract(10, 5), 5)
+        self.assertEqual(calc.subtract(5, 10), -5)
+        self.assertEqual(calc.subtract(10, 5), 5)
+        self.assertEqual(calc.subtract(5, 10), -5)
